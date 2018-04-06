22fad666aaac2dc554d2827ed24a4a149f349dea
Ben Mather <bwhmather@bwhmather.com>
2018-04-06 22:46:53 +0100

    Docstrings for the easy functions

--- include/lsp.h
+++ include/lsp.h
@@ -38,7 +38,8 @@ bool lsp_is_equal();
 
 
 /**
- * Integer operations.
+ * Integers
+ * --------
  */
 void lsp_push_int(int value);
 bool lsp_is_int();
@@ -51,39 +52,144 @@ void lsp_int_div();
 
 
 /**
- * Symbols operations.
+ * Symbols
+ * -------
  */
 void lsp_push_symbol(char *value);
 bool lsp_is_symbol();
 char *lsp_borrow_symbol();
+bool lsp_symbol_matches_literal(const char *value);
 
 
 /**
- * String operations.
+ * String
+ * ------
+ */
+
+/**
+ * Copies a null terminated string onto the heap and pushes a reference to it
+ * onto the stack.
  */
 void lsp_push_string(char *value);
-bool lsp_is_string();
-char *lsp_borrow_string();
-
-void lsp_str_len();
-void lsp_str_concat();
-
 
 /**
- * Basic cons cell operations.
+ * Pops a reference from the top of the stack, returning true if it points to a
+ * string and false otherwise.
+ *
+ * Arguments:
+ *   - value: The value to check for stringiness.
+ */
+bool lsp_is_string();
+
+/**
+ * Returns a temporary reference to a null terminated string stored on the
+ * heap.
+ *
+ * The reference pointing to the string will **not** be popped from the stack.
+ *
+ * .. warning::
+ *     Calling any lsp function other than `lsp_borrow_string` is could result
+ *     in a garbage collection that would invalidate the reference..
+ */
+char *lsp_borrow_string();
+
+/**
+ * Pops a string and replaces it with an integer specifying its length.
+ *
+ * Arguments:
+ *   - string: The string we would like to find the length for.
+ *
+ * Returns:
+ *   - length: The length of the string not including the terminating null.
+ */
+void lsp_str_len();
+
+/**
+ * Pops a pair of strings from the stack and returns a new 
+ */
+void lsp_str_concat();
+
+/**
+ * Cons cells
+ * ----------
+ */
+
+/**
+ * Pushes a new cons cell on to the top of the stack with both car and cdr set
+ * to null.
  */
 void lsp_push_cons();
+
+/**
+ * Pops a value from the stack, returning true if it is a cons cell and false
+ * otherwise.
+ *
+ * Arguments:
+ *   - The value we want to check is a cons cell.
+ */
 bool lsp_is_cons();
 
+/**
+ * Pops two values from the top of the stack and wraps them in a cons cell.
+ *
+ * Arguments:
+ *   - car: the value to save as the first value in the cons cell.
+ *   - cdr: the value to save as the second value in the cons cell.
+ *
+ * Returns:
+ *   - cons: the new cons cell.
+ */
 void lsp_cons();
+
+/**
+ * Returns the first value in a cons cell.
+ *
+ * Arguments:
+ *   - cons: A cons cell to extract the first element from.
+ *
+ * Returns:
+ *   - The first value in the cons cell.
+ */
 void lsp_car();
+
+/**
+ * Returns the second value in a cons cell.
+ *
+ * Arguments:
+ *   - cons: A cons cell to extract the second element from.
+ *
+ * Returns:
+ *   - The second value in the cons cell.
+ */
 void lsp_cdr();
+
+/**
+ * Sets value pointed to by the car of an existing cons cell and pops both the
+ * replacement value and the cons cell from the stack.
+ *
+ * Arguments:
+ *   - cons: The cons cell to modify.
+ *   - value: The value to replace the first value in the cons cell with.
+ */
 void lsp_set_car();
 void lsp_set_cdr();
 
 
 /**
- * Higher order list operations.
+ * Higher order list operations
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+/**
+ * Applies a callable object to each item in a list and returns a new list
+ * containing the results.
+ *
+ * Arguments:
+ *   - fn: The operation to apply.
+ *   - list: The list of values to apply the operation to.
+ *
+ * Returns:
+ *   - A new list of the return values..
  */
 void lsp_map();
 void lsp_fold();
@@ -93,8 +199,22 @@ void lsp_reverse();
 /**
  * Managing environments for function execution.
  */
+
+/**
+ * Creates a new empty scope that inherits from the current environment.
+ */
 void lsp_push_scope();
+
+/**
+ * Creates a new binding in the inner-most scope of the current environment.
+ */
 void lsp_define();
+
+/**
+ * Returns the value bound to a symbol in the current scope.
+ *
+ * Will abort if no binding exists.
+ */
 void lsp_lookup();
 void lsp_set();
 void lsp_push_empty_env();
@@ -108,6 +228,7 @@ void lsp_vm_init();
 
 void lsp_parse();
 
+void lsp_call();
 void lsp_eval();
 
 void lsp_print();
