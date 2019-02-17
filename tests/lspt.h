82c27965a93464ba3b0c4468a42aa1c7142d0028
Ben Mather <bwhmather@bwhmather.com>
2019-02-17 22:48:02 +0000

    First pass at implementing assertion macros

--- tests/lspt.h
+++ tests/lspt.h
@@ -1,22 +1,31 @@
 #include <stdlib.h>
 #include <signal.h>
 
-static void on_sigabrt(int signal) {
+#define LSPT_EXPAND(x) x
+
+#define lspt_assert(condition) do { \
+    bool lspt_internal_condition = !!(condition); \
+    /* char *lspt_internal_lineno = __LINE__; */ \
+    /* char *lspt_internal_file = __FILE__; */ \
+    if (!condition) { \
+        abort(); \
+    } \
+} while (0)
+
+
+static void lspt_internal_on_sigabrt(int signal) {
     exit(0);
 }
 
 #define lspt_assert_aborts(expr) do { \
-    signal(SIGABRT, &on_sigabrt); \
+    signal(SIGABRT, &lspt_internal_on_sigabrt); \
     expr; \
     exit(1); \
 } while(0)
 
+#define lspt_assert_not(condition) LSPT_EXPAND(lspt_assert(!(condition)))
+#define lspt_assert_eq(actual, expected) LSPT_EXPAND(lspt_assert(((actual) == (expected))))
 
-#define lspt_assert(condition)
-#define lspt_assert_not(condition)
-#define lspt_assert_eq(actual, expected)
-
-
-#define lspt_expect(condition) lspt_assert(condition)
-#define lspt_expect_not(condition) lspt_expect(condition)
-#define lspt_expect_eq(actual, expected) lspt_expect_eq(a, b)
+#define lspt_expect(condition) LSPT_EXPAND(lspt_assert((condition)))
+#define lspt_expect_not(condition) LSPT_EXPAND(lspt_assert((condition)))
+#define lspt_expect_eq(actual, expected) LSPT_EXPAND(lspt_assert_eq((actual), (expected)))
