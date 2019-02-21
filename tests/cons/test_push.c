874f20968dd46372da88f4515c51fca199b12e34
Ben Mather <bwhmather@bwhmather.com>
2019-02-21 08:00:41 +0000

    Check that cons cell added by `lsp_push_cons` is empty

--- tests/cons/test_push.c
+++ tests/cons/test_push.c
@@ -9,7 +9,17 @@ int main(int argc, char **argv) {
     lsp_push_cons();
 
     lspt_assert_eq(lsp_stats_frame_size(), 1);
+
+    lsp_dup(-1);
     lspt_assert(lsp_is_cons());
+
+    lsp_dup(-1);
+    lsp_car();
+    lspt_assert(lsp_is_null());
+
+    lsp_dup(-1);
+    lsp_cdr();
+    lspt_assert(lsp_is_null());
 
     return 0;
 }
