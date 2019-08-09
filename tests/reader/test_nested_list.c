7c973153dbff482108ca782b477a8c5ac1b09f21
Ben Mather <bwhmather@bwhmather.com>
2019-08-09 08:56:22 +0100

    Add test for nested lists

---
+++ tests/reader/test_nested_list.c
@@ -0,0 +1,90 @@
+/**
+ * Checks that `lsp_parse` can read nested lists.
+ */
+#include "lsp.h"
+
+#include "lspt.h"
+
+
+int main(int argc, char **argv) {
+    lsp_vm_init();
+
+    lsp_push_string("((1 2) () 3 (4))");
+    lsp_parse();
+
+    lspt_expect(lsp_stats_frame_size() == 1);
+
+    lsp_dup(0);
+    lsp_car();
+    /* BEGIN */
+    lsp_dup(0);
+    lsp_car();
+    /* BEGIN */
+    lsp_dup(0);
+    lsp_car();
+    lspt_assert(lsp_read_int(0) == 1);
+    lsp_pop();
+
+    lsp_cdr();
+
+    lsp_dup(0);
+    lsp_car();
+    lspt_assert(lsp_read_int(0) == 2);
+    lsp_pop();
+
+    lsp_cdr();
+
+    lsp_dup(0);
+    lspt_assert(lsp_is_null());
+    lsp_pop();
+    /* END */
+    lsp_pop();
+
+    lsp_cdr();
+
+    lsp_dup(0);
+    lsp_car();
+    lspt_assert(lsp_is_null());
+    lsp_pop();
+
+    lsp_cdr();
+
+    lsp_dup(0);
+    lsp_car();
+    lspt_assert(lsp_read_int(0) == 3);
+    lsp_pop();
+
+    lsp_cdr();
+
+    lsp_dup(0);
+    lsp_car();
+    /* BEGIN */
+    lsp_dup(0);
+    lsp_car();
+    lspt_assert(lsp_read_int(0) == 4);
+    lsp_pop();
+
+    lsp_cdr();
+
+    lsp_dup(0);
+    lspt_assert(lsp_is_null());
+    lsp_pop();
+    /* END */
+    lsp_pop();
+
+    lsp_cdr();
+
+    lsp_dup(0);
+    lspt_assert(lsp_is_null());
+    lsp_pop();
+    /* END */
+    lsp_pop();
+
+    lsp_cdr();
+
+    lsp_dup(0);
+    lspt_expect(lsp_is_null());
+    lsp_pop();
+
+    return 0;
+}
