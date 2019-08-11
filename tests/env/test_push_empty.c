6a270170f75a35c5b32ea4676aa9bd5f1e19892d
Ben Mather <bwhmather@bwhmather.com>
2019-08-11 09:37:38 +0100

    Add test for pushing an empty environment

---
+++ tests/env/test_push_empty.c
@@ -0,0 +1,28 @@
+/**
+ * Checks that `lsp_parse` can read nested lists.
+ */
+#include "lsp.h"
+
+#include "lspt.h"
+
+
+int main(int argc, char **argv) {
+    lsp_vm_init();
+
+    lsp_push_empty_env();
+
+    lspt_assert(lsp_stats_frame_size() == 1);
+
+    lsp_dup(0);
+    lsp_car();
+    lspt_assert(lsp_is_null());
+    lsp_pop();
+
+    lsp_cdr();
+
+    lsp_dup(0);
+    lspt_assert(lsp_is_null());
+    lsp_pop();
+
+    return 0;
+}
