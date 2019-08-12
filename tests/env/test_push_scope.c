438a85198e8f887c1694b38ebbb7390e6354d0d0
Ben Mather <bwhmather@bwhmather.com>
2019-08-12 21:38:05 +0100

    Add test for pushing an empty scope onto an env

---
+++ tests/env/test_push_scope.c
@@ -0,0 +1,40 @@
+/**
+ * Checks that `lsp_push_scope` will add a new frame to the environment.
+ */
+#include "lsp.h"
+
+#include "lspt.h"
+
+
+int main(void) {
+    lsp_vm_init();
+
+    lsp_push_empty_env();
+
+    lspt_assert(lsp_stats_frame_size() == 1);
+
+    lsp_push_scope();
+
+    // Current scope should be empty.
+    lsp_dup(0);
+    lsp_car();
+    lspt_assert(lsp_is_null());
+    lsp_pop();
+
+    lsp_cdr();
+
+    // Parent scope should be empty.
+    lsp_dup(0);
+    lsp_car();
+    lspt_assert(lsp_is_null());
+    lsp_pop();
+
+    lsp_cdr();
+
+    // Terminating null.
+    lsp_dup(0);
+    lspt_assert(lsp_is_null());
+    lsp_pop();
+
+    return 0;
+}
